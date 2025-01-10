fb7ebd1db7933c19f6f6ec72f5dc7fa7187302d0
Luca Pandolfini <l.pandolfini@sintraconsulting.eu>
2025-01-10 18:08:18 +0100

    Updated Auth Test

--- src/test/java/com/myvg/myvg/AuthTest.java
+++ src/test/java/com/myvg/myvg/AuthTest.java
@@ -1,57 +1,63 @@
 package com.myvg.myvg;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 import com.myvg.myvg.DTO.UserDTO;
 import com.myvg.myvg.DTO.VideogameDTO;
 import com.myvg.myvg.Controllers.LoginController;
+import com.myvg.myvg.DAO.UserDAO;
 import com.myvg.myvg.DTO.ReviewDTO;
 import com.myvg.myvg.Services.AppContext;
 import com.myvg.myvg.Services.UserService;
+import com.myvg.myvg.EntityModel.UserEntity;
 
-import java.util.List;
-
+//@SpringBootTest
 public class AuthTest {
-
-    @Autowired
-    private LoginController loginController;
-
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private AppContext appContext;
+
     private UserDTO testUser;
+    private UserEntity usrEnt;
 
     @BeforeEach
     public void setUp() {
-        testUser = new UserDTO("Test User", "password", 1, List.of(
-            new VideogameDTO("Test Game 1", "Test Genre", 2000, List.of("Test Platform"), List.of(
-                new ReviewDTO("Test User", "Test Game", 10, "Test Comment", 3),
-                new ReviewDTO("Test User", "Test Game", 10, "Test Comment", 3)
-            )),
-            new VideogameDTO("Test Game 2", "Test Genre", 2000, List.of("Test Platform"), List.of(
-                new ReviewDTO("Test User", "Test Game", 10, "Test Comment", 3),
-                new ReviewDTO("Test User", "Test Game", 10, "Test Comment", 3)
-            ))
-        ));
+        testUser = new UserDTO("Muccarini", "example@gmail.com", 0, null);
     }
 
     @Test
-    public void testRegisterAndLogin() {
-        loginController.register("Test User", "password", "email@gmail.com");
-        loginController.login("Test User", "password");
+    public void testHandleRegister() throws Exception {
+        userService.registerUser(testUser.getUsername(), "password", testUser.getEmail(), testUser.getAvatarID());
 
-        assertEquals("Test User", AppContext.getInstance().getCurrentUser().getUsername());
-    }
-    
-    @AfterEach
-    public void tearDown() {
-        userService.removeUser(testUser.getUsername(), "password");
-        assertNull(AppContext.getInstance().getCurrentUser());
+        // Check the database for the user
+        UserDTO registeredUser = userService.getUserByUsername("Muccarini");
+        assertNotNull(registeredUser);
+        assertEquals("Muccarini", registeredUser.getUsername());
+
+        userService.loginUser("Muccarini", "password");
+        assertEquals("Muccarini", appContext.getCurrentUser().getUsername());
     }
 
+    // @AfterEach
+    // public void tearDown() {
+    //     userDAO.delete(usrEnt);
+    //     assertNull(AppContext.currentUser());
+    // }
 }
